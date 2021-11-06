5054a801e09cac8a3820ae7c5ae11f1452cbdd5c
ashleyn <ashleynavo@googlemail.com>
2021-11-06 18:51:36 +0000

    added code/tests for port & itinerary

--- src/Ship.js
+++ src/Ship.js
@@ -1,14 +1,28 @@
 //const startPoint = "Dover";
 
 class Ship {
-  constructor(startPoint) {
-    this.currentPort = startPoint;
+  constructor(itinerary) {
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.previousPort = null;
   }
   setSail() {
-    return (this.currentPort = null);
+    //return (this.currentPort = null);
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+    if (currentPortIndex === itinerary.ports.length - 1) {
+      throw new Error("End of itinerary reached");
+    }
+
+    this.previousPort = this.currentPort;
+    this.currentPort = null;
   }
   dock(port) {
-    this.currentPort = port;
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
   }
 }
 
